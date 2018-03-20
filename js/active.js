e0c1a3d47dea5eddf6b1a6f53b6ef06f7b05e14c
Pierre Facq <zoruninho@gmail.com>
2018-03-20 18:47:35 +0100

    Cleanup

--- js/active.js
+++ js/active.js
@@ -2,9 +2,7 @@
     'use strict';
 
     // [ JS Active Code Index ]
-
-    // :: 1.0 Owl Carousel Active Code
-    // :: 2.0 Slick Active Code
+    
     // :: 3.0 Footer Reveal Active Code
     // :: 4.0 ScrollUp Active Code
     // :: 5.0 CounterUp Active Code
@@ -12,66 +10,6 @@
     // :: 7.0 Magnific-popup Video Active Code
     // :: 8.0 Sticky Active Code
     // :: 9.0 Preloader Active code
-
-    // :: 1.0 Owl Carousel Active Code
-    if ($.fn.owlCarousel) {
-        $(".welcome_slides").owlCarousel({
-            items: 1,
-            loop: true,
-            autoplay: true,
-            smartSpeed: 1500,
-            nav: true,
-            navText: ["<i class='pe-7s-angle-left'</i>", "<i class='pe-7s-angle-right'</i>"]
-        });
-        $(".app_screenshots_slides").owlCarousel({
-            items: 1,
-            loop: true,
-            autoplay: true,
-            smartSpeed: 800,
-            margin: 30,
-            center: true,
-            dots: true,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                480: {
-                    items: 3
-                },
-                992: {
-                    items: 5
-                }
-            }
-        });
-    }
-
-    // :: 2.0 Slick Active Code
-    if ($.fn.slick) {
-        $('.slider-for').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            speed: 500,
-            arrows: false,
-            fade: true,
-            asNavFor: '.slider-nav'
-        });
-        $('.slider-nav').slick({
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            speed: 500,
-            asNavFor: '.slider-for',
-            dots: true,
-            centerMode: true,
-            focusOnSelect: true,
-            slide: 'div',
-            autoplay: true,
-            centerMode: true,
-            centerPadding: '30px',
-            mobileFirst: true,
-            prevArrow: '<i class="fa fa-angle-left"></i>',
-            nextArrow: '<i class="fa fa-angle-right"></i>'
-        });
-    }
 
     // :: 3.0 Footer Reveal Active Code
     if ($.fn.footerReveal) {
